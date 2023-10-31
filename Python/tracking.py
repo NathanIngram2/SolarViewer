b5bad3178bdae5705be0c891d8c7e2b70e2627a0
Devynn Garrow <devynn.garrow@outlook.com>
2023-10-31 13:22:55 -0400

    Allowing for integration with 2 stepper motors connected to Pi

--- Python/tracking.py
+++ Python/tracking.py
@@ -11,8 +11,10 @@ from astropy.time import Time
 from astropy.coordinates import get_sun, AltAz, EarthLocation
 from utilities import Log
 
-DIR = 10 # Direction pin from controller
-STEP = 8 # Step pin from controller
+DIR_AZ = 10 # Azimuth stepper motor direction pin from controller
+STEP_AZ = 8 # Azimuth stepper motor pin from controller
+DIR_ALT = 13 # Altitude stepper motor direction pin from controller
+STEP_ALT = 15 # Altitude stepper motor direction pin from controller
 LIM_ALT = 7 # Limit switch 1 input from Pi
 LIM_AZ = 11 # Limit switch 2 input from Pi
 STEP_SIZE = 1.8 # Nema 23 Stepper Motor (1.8 step angle, 200 steps per revolutions)
@@ -21,11 +23,14 @@ CW = 1 # 0/1 used to signify clockwise or counterclockwise.
 CCW = 0
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(DIR, GPIO.OUT) # Establish Pins in software
-GPIO.setup(STEP, GPIO.OUT)
+GPIO.setup(DIR_AZ, GPIO.OUT) # Establish Pins in software
+GPIO.setup(STEP_AZ, GPIO.OUT)
+GPIO.setup(DIR_ALT, GPIO.OUT)
+GPIO.setup(STEP_ALT, GPIO.OUT)
 GPIO.setup(LIM_ALT, GPIO.IN)
 GPIO.setup(LIM_AZ, GPIO.IN)
-GPIO.output(DIR, CW) # Direction of starting spin
+GPIO.output(DIR_AZ, CCW) # Direction of calibration spin for azimuth stepper motor
+GPIO.output(DIR_ALT, CCW) # Direction of calibration spin for altitude stepper motor
 
 def calibrate(offset):
     """
@@ -35,7 +40,6 @@ def calibrate(offset):
     :return: Starting altitude and azimuth of the antenna in degrees. (0,0)
     """
     Log.info("Starting Calibration...")
-    GPIO.output(DIR, CCW)
     # Altitude calibration
     while(GPIO.input(LIM_ALT) != GPIO.HIGH):
         moveStepper(CALIBRATION_ROTATION_SIZE, 0)
@@ -44,7 +48,8 @@ def calibrate(offset):
     while(GPIO.input(LIM_AZ) != GPIO.HIGH):
         moveStepper(0, CALIBRATION_ROTATION_SIZE)
 
-    GPIO.output(DIR, CW)
+    GPIO.output(DIR_AZ, CW)
+    GPIO.output(DIR_ALT, CW)
     Log.info("Done Calibration.")
     return 0, offset
 
@@ -112,9 +117,8 @@ def moveStepper(diffAlt, diffAz):
             time.sleep(0.001)  # TODO: Adjust sleep time.
 
     # Rotate the stepper motor for altitude and azimuth.
-    rotateMotor(STEP, stepsAlt)
-    # TODO: Implement azimuthal control.
-    # rotateMotor(STEP, diffAz)
+    rotateMotor(STEP_ALT, stepsAlt)
+    rotateMotor(STEP_AZ, stepsAz)
 
     Log.info("Stepper motor moved. Altitude change: " + str(diffAlt) + " degrees, Azimuth change: " + str(diffAz) + " degrees")
     Log.info("Done moveStepper")
--- exp/test_limit_switches.py
+++ exp/test_limit_switches.py
@@ -1,12 +1,11 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-# Direction pin from controller
-DIR = 10
-# Step pin from controller
-STEP = 8
-# 0/1 used to signify clockwise or counterclockwise.
-CW = 1
+DIR_AZ = 10 # Azimuth stepper motor direction pin from controller
+STEP_AZ = 8 # Azimuth stepper motor pin from controller
+DIR_ALT = 13 # Altitude stepper motor direction pin from controller
+STEP_ALT = 15 # Altitude stepper motor direction pin from controller
+CW = 1 # 0/1 used to signify clockwise or counterclockwise.
 CCW = 0
 
 LIM_ALT = 7 # Limit switch 1 input from Pi
@@ -16,13 +15,16 @@ LIM_AZ = 11 # Limit switch 2 input from Pi
 GPIO.setmode(GPIO.BOARD)
 
 # Establish Pins in software
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
+GPIO.setup(DIR_AZ, GPIO.OUT)
+GPIO.setup(STEP_AZ, GPIO.OUT)
+GPIO.setup(DIR_ALT, GPIO.OUT)
+GPIO.setup(STEP_ALT, GPIO.OUT)
 GPIO.setup(LIM_ALT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LIM_AZ, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Set the first direction you want it to spin
-GPIO.output(DIR, CW)
+GPIO.output(DIR_AZ, CCW) # Direction of calibration spin for azimuth stepper motor
+GPIO.output(DIR_ALT, CCW) # Direction of calibration spin for altitude stepper motor
 
 def calibrate(offset):
 	"""
@@ -32,36 +34,31 @@ def calibrate(offset):
 	:return: Starting altitude and azimuth of the antenna in degrees. (0,0)
 	"""
 	print("Starting Calibration...")
-	GPIO.output(DIR, CCW)
 	# Altitude calibration
 	while(GPIO.input(LIM_ALT) != GPIO.HIGH):
-		# Run for 200 steps. This will change based on how you set you controller
-		for x in range(10):
-			# Set one coil winding to high
-			GPIO.output(STEP,GPIO.HIGH)
-			# Allow it to get there.
-			sleep(.005) # Dictates how fast stepper motor will run
-			# Set coil winding to low
-			GPIO.output(STEP,GPIO.LOW)
-			sleep(.005) # Dictates how fast stepper motor will run
+        # Set one coil winding to high
+        GPIO.output(STEP_ALT,GPIO.HIGH)
+        # Allow it to get there.
+        sleep(.005) # Dictates how fast stepper motor will run
+        # Set coil winding to low
+        GPIO.output(STEP_ALT,GPIO.LOW)
+        sleep(.005) # Dictates how fast stepper motor will run
 		
 
 	sleep(5) # Dictates how fast stepper motor will run
-	GPIO.output(DIR, CW)
 
 	# Azimuth calibration
 	while(GPIO.input(LIM_AZ) != GPIO.HIGH):
-		# Run for 200 steps. This will change based on how you set you controller
-		for x in range(10):
-			# Set one coil winding to high
-			GPIO.output(STEP,GPIO.HIGH)
-			# Allow it to get there.
-			sleep(.005) # Dictates how fast stepper motor will run
-			# Set coil winding to low
-			GPIO.output(STEP,GPIO.LOW)
-			sleep(.005) # Dictates how fast stepper motor will run
+        # Set one coil winding to high
+        GPIO.output(STEP_AZ,GPIO.HIGH)
+        # Allow it to get there.
+        sleep(.005) # Dictates how fast stepper motor will run
+        # Set coil winding to low
+        GPIO.output(STEP_AZ,GPIO.LOW)
+        sleep(.005) # Dictates how fast stepper motor will run
 
-	GPIO.output(DIR, CCW)
+	GPIO.output(DIR_AZ, CW)
+    GPIO.output(DIR_ALT, CW)
 	return 0, offset
 
 
