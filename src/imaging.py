676a4b5c2fb60a51e8f5a7d85db54d2afee58e26
Ben Graham <ben.n.graham@icloud.com>
2023-11-22 17:56:54 -0500

    Add imaging support in imaging.py

--- src/imaging.py
+++ src/imaging.py
@@ -6,7 +6,42 @@ Description: Data collection and processing methods for the Solar Eclipse Viewer
 for ENPH454 @ Queen's University, Kingston ON.
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import datetime
+
+from dataCollection import measPower
+from tracking import calibrate, moveStepper
+from utilities import Log
+
 # TODO: Move to its own folder, image.py should have its own main.
 
 # TODO: Implement a script to take an image by sweeping over a input range of Al and Az angles,
 #  plot measurement as a heatmap (Az on x, Al on y).
+
+# Setup Logging
+log = Log("HIGH")
+
+FREQ_MIN = "980M"
+FREQ_MAX = "1020M"
+INTEGRATION_INTERVAL = "1s"
+GAIN = "0"
+ANT_OFFSET_EL = 0
+
+AZ_MAX = 120
+EL_MAX = 60
+
+calibrate(ANT_OFFSET_EL)
+
+data = np.zeros((EL_MAX, AZ_MAX))
+
+for i in range(EL_MAX):
+    for j in range(AZ_MAX):
+        data[i][j] = measPower(FREQ_MIN, FREQ_MAX, INTEGRATION_INTERVAL, GAIN)
+        moveStepper(1, 1)
+        time.sleep(1)
+
+np.savetxt("ImageData" + str(datetime.datetime.now().strftime("%Y%m%d%H%M%S")) + ".csv", data, delimiter=",")
+plt.imshow(data, origin='lower', interpolation=None)
+plt.show()
