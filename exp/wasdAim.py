52c280bf00811b9150c536ef1b3cd8bc0fcb9ab7
Devynn Garrow <devynn.garrow@outlook.com>
2023-11-28 11:55:04 -0500

    Adding output of angle during manual pointing based on manual 0 point

--- exp/wasdAim.py
+++ exp/wasdAim.py
@@ -17,6 +17,13 @@ STEP_ALT = 15 # Altitude stepper motor direction pin from controller
 CW = 1
 CCW = 0
 
+STEP_SIZE = 0.45
+EL_GEAR_RATIO = 10
+AZ_GEAR_RATIO = 4.4
+
+DEGREES_EL = STEP_SIZE/EL_GEAR_RATIO
+DEGREES_AZ = STEP_SIZE/AZ_GEAR_RATIO
+
 # board and pin setup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(DIR_AZ, GPIO.OUT)
@@ -26,13 +33,22 @@ GPIO.setup(STEP_ALT, GPIO.OUT)
 
 print("Enter w-a-s-d for respective movement of the antenna direction.")
 print("W: increase altitude, A: rotate CCW, S: decrease altitude, D: rotate CW")
+zeroPoint = False
 
 while True:
 
     keyPressed = getch()
 
+    if keyPressed == 'z':
+        zeroPoint = True
+        az = 0
+        alt = 0
+
     if keyPressed == 'w':
         print("Increasing altitude")
+        if zeroPoint == True:
+                alt = alt + DEGREES_EL
+                print("Updated location: Alt = " + alt + " Az = " + az)
         GPIO.output(DIR_ALT,CW)
         # Set one coil winding to high
         GPIO.output(STEP_ALT,GPIO.HIGH)
@@ -44,7 +60,10 @@ while True:
 
     if keyPressed == 'a':
         print("CCW rotation")
-        GPIO.output(DIR_AZ, CCW)
+        if zeroPoint == True:
+                az = az - DEGREES_AZ
+                print("Updated location: Alt = " + alt + " Az = " + az)
+        GPIO.output(DIR_AZ, CW)
         # Set one coil winding to high
         GPIO.output(STEP_AZ,GPIO.HIGH)
         # Allow it to get there.
@@ -55,6 +74,9 @@ while True:
 
     if keyPressed == 's':
         print("Decreasing altitude")
+        if zeroPoint == True:
+                alt = alt - DEGREES_EL
+                print("Updated location: Alt = " + alt + " Az = " + az)
         GPIO.output(DIR_ALT, CCW)
         # Set one coil winding to high
         GPIO.output(STEP_ALT,GPIO.HIGH)
@@ -66,7 +88,10 @@ while True:
 
     if keyPressed == 'd':
         print("CW rotation")
-        GPIO.output(DIR_AZ, CW)
+        if zeroPoint == True:
+                az = az + DEGREES_AZ
+                print("Updated location: Alt = " + alt + " Az = " + az)
+        GPIO.output(DIR_AZ, CCW)
         # Set one coil winding to high
         GPIO.output(STEP_AZ,GPIO.HIGH)
         # Allow it to get there.
