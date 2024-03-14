60acda73f38edb679d1b849787837daca8f2348d
Ben Graham <ben.n.graham@icloud.com>
2024-03-13 21:46:02 -0400

    Add GNU GPLv3 license

--- src/main.py
+++ src/main.py
@@ -3,6 +3,22 @@ File: main.py
 Author: Will Conway, Devynn Garrow, Ben Graham, Jessica Guetre, Nathan Ingram
 Date: 2023-10-25
 Description: Main program for the Solar Eclipse Viewer project for ENPH454 @ Queen's University, Kingston ON.
+
+
+Copyright (C) 2023  Will Conway, Devynn Garrow, Ben Graham, Jessica Guetre, Nathan Ingram
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 # Package imports
